fcb2ebac954c08f64092c6f2c91a524b49044c79
melane1978 <melane1978@gmail.com>
2018-10-03 20:30:46 +0200

    added my first javafx script

---
+++ src/main/java/test.java
@@ -0,0 +1,25 @@
+package main.java;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class test extends Application {
+
+    @Override
+        public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+
+	 Label label = new Label("Hello World, JavaFX !");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+
+
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+
+}
