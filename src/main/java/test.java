22d956bded7b9d24db5bbec1ab4eb5c820e1f462
melane1978 <melane1978@gmail.com>
2018-10-04 18:07:43 +0200

    added changes

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,25 +1,24 @@
 package main.java;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class test extends Application {
 
     @Override
-        public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("My First JavaFX App");
-
-	 Label label = new Label("Hello World, JavaFX !");
+        
+        Label label = new Label("Hello World, JavaFX !");
         Scene scene = new Scene(label, 400, 200);
         primaryStage.setScene(scene);
-
-
+        
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         Application.launch(args);
     }
-    
-
 }
